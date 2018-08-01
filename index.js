d9e0e927b06443ef9bc184ba5bd3ae9fff460f3f
aleadinglight <huutam.ptnk@gmail.com>
2018-08-01 15:38:58 +0700

    added slider

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+var slider;
+var angle;
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(800, 800);
+    slider = createSlider(0, TWO_PI, PI/4, 0.01);
 }
 
 function draw(){
     background(51);
-    var len = 100;
+    angle = slider.value();
     stroke(255);
-    line(200, height, 200, height - len);
+    translate(width/2,height);
+    branch(200);
+}
+
+function branch(len){
+    // Draw a line with length len
+    line(0, 0, 0, -len);
+    // The origin is changed to the new end point
+    translate(0,-len);
+    // rotate to draw another branch
+    rotate(angle);
+    // Recursive call if len is more than 4 pixels
+    if (len>4) {
+        branch(len*0.67);
+    }
 }
