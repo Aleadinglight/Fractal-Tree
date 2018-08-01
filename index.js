5c1b515e2ed8413b4c9859e8199bd7958300ed53
aleadinglight <huutam.ptnk@gmail.com>
2018-08-01 15:57:20 +0700

    put the canvas into div tag

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var slider;
-var angle;
+var slider=0;
+var angle=0;
+
 function setup() {
-    createCanvas(800, 800);
+    var canvas = createCanvas(700, 700);
+    // Move the canvas so it’s inside our <div id="sketch-holder">.
+    canvas.parent('sketch-holder');
     slider = createSlider(0, TWO_PI, PI/4, 0.01);
 }
 
@@ -18,10 +21,18 @@ function branch(len){
     line(0, 0, 0, -len);
     // The origin is changed to the new end point
     translate(0,-len);
-    // rotate to draw another branch
-    rotate(angle);
+    
     // Recursive call if len is more than 4 pixels
     if (len>4) {
+        push();
+        // rotate to draw another branch
+        rotate(angle);
         branch(len*0.67);
+        pop();
+        push();
+        // rotate again to draw another branch
+        rotate(-angle);
+        branch(len*0.67);
+        pop();
     }
 }
